4c51cb12218aa56d4ee96860f0f77248e788c4d8
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2021-03-09 21:46:42 -0800

    rename lastIndexToChooseFrom to totalItemsToChooseFrom

--- index.js
+++ index.js
@@ -20,20 +20,20 @@ const selectNWords = n => {
     for (let i = 0; i < n; i += 1)
         {
             const randomDigit = Math.floor(Math.random() * 10);
-            let lastIndexToChooseFrom = 1000;
+            let totalItemsToChooseFrom = 1000;
 
             // weight choices
             // // 30% chance of selecting from first 10 words
             // if (randomDigit < 3)
-            //     { lastIndexToChooseFrom = 10; }
+            //     { totalItemsToChooseFrom = 10; }
             // // 40% chance of selecting from first 100 words
             // else if (randomDigit < 7)
-            //     { lastIndexToChooseFrom = 100; }
+            //     { totalItemsToChooseFrom = 100; }
             // // 30% chance of selecting from first 1000 words
             // else
-            //     { lastIndexToChooseFrom = 1000; }
+            //     { totalItemsToChooseFrom = 1000; }
 
-            wordsChosen[i] = top1000[Math.floor(Math.random() * lastIndexToChooseFrom)];
+            wordsChosen[i] = top1000[Math.floor(Math.random() * totalItemsToChooseFrom)];
         }
 
     return wordsChosen;
