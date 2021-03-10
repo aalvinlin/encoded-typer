c085ceca57b767e48b9b754c58e983807aa0c17c
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2021-03-09 21:47:15 -0800

    remove weighting of random words during selection

--- index.js
+++ index.js
@@ -19,20 +19,7 @@ const selectNWords = n => {
 
     for (let i = 0; i < n; i += 1)
         {
-            const randomDigit = Math.floor(Math.random() * 10);
             let totalItemsToChooseFrom = 1000;
-
-            // weight choices
-            // // 30% chance of selecting from first 10 words
-            // if (randomDigit < 3)
-            //     { totalItemsToChooseFrom = 10; }
-            // // 40% chance of selecting from first 100 words
-            // else if (randomDigit < 7)
-            //     { totalItemsToChooseFrom = 100; }
-            // // 30% chance of selecting from first 1000 words
-            // else
-            //     { totalItemsToChooseFrom = 1000; }
-
             wordsChosen[i] = top1000[Math.floor(Math.random() * totalItemsToChooseFrom)];
         }
 
